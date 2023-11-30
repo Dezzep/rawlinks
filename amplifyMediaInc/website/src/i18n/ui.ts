62ee55cc060d1ea1dc15fcf54f52831ab96cc4ed
Jacob Handelman <jacobhandelman@gmail.com>
2023-11-30 17:38:31 -0500

    edit readme

---
+++ amplifyMediaInc/website/src/i18n/ui.ts
@@ -0,0 +1,95 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+
+    // BUTTONS
+    'btn.callToAction': 'Book a free consultation',
+    'btn.learnMore': 'Learn more',
+    
+    // NAVIGATION
+    'nav.home': 'Home',
+    'nav.contact': 'Contact us',
+    'nav.lang': 'FR',
+
+    //MISSION STATEMENT SECTION
+
+    'mission.title': 'Mission Statement',
+    'mission.subtitle':
+      'At Amplify, our mission is to provide top-tier customer service, ensuring utmost satisfaction. We forge enduring relationships based on trust and transparency, while our unwavering commitment to excellence guarantees financial success for all. Empowering businesses to thrive, we transform visions into reality, creating a community where success has no limits.',
+
+    // SERVICES SECTION
+
+    'services.header': 'What Services do we Provide?',
+    'services.calltoaction': 'Read more',
+
+    'services.title1': 'Facebook/Instagram Advertising',
+    'services.description1':
+      'We create custom social media advertising campaigns that target the right audience and drive maximum engagement and conversions.',
+
+    'services.title2': 'Content Creation',
+    'services.description2':
+      'We will Boost your SEO rankings, help you foster authentic connections with your audience, our service is the key to thriving in the competitive online landscape.',
+
+    'services.title3': 'Website Creation',
+    'services.description3':
+      'We design and build websites that prioritize high-quality content, engage user experiences, and utilize effective SEO strategies to attract and retain audiences.',
+
+    // GOOD AT SECTION
+
+    'good.header': 'Strategic Solutions for Digital Success',
+    'good.subtitle': 'Elevate, Generate and Thrive with Our Expert Services',
+
+    'good.title1': 'Strategic Social Media Management',
+    'good.description1':
+      'Elevate your online presence with our strategic social media management, where we craft engaging content and leverage targeted advertising to amplify brand visibility and drive meaningful interactions.',
+
+    'good.title2': 'Comprehensive Content Creation',
+    'good.description2':
+      "Our comprehensive content creation service ensures your brand's voice is heard, incorporating bespoke strategies, diverse multimedia production, and SEO optimization to captivate audiences and boost online visibility.",
+
+    'good.title3': 'Results-Driven Lead Generation',
+    'good.description3':
+      'Drive business growth with our results-driven lead generation services, leveraging advanced strategies on platforms like Facebook and Instagram to capture and convert potential customers effectively.',
+
+    'good.title4': 'Website Creation',
+    'good.description4':
+      'Architecting digital experiences, our holistic website creation services redefine your online presence, crafting bespoke websites that stand out, captivate audiences, and align seamlessly with your brand identity.',
+
+    'good.title5': 'Data-Driven Analytics & Optimization',
+    'good.description5':
+      'Maximize performance with our data-driven approach, employing meticulous analytics and continuous optimization to refine strategies and ensure your digital marketing efforts consistently surpass expectations.',
+
+    'good.title6': 'Expert Consultation & Collaboration',
+    'good.description6':
+      'Benefit from our expertise with personalized consultation and collaboration, where our team of specialists provides insights into industry trends, emerging strategies, and creative solutions to help your business thrive in the digital',
+
+    // OUR TEAM SECTION
+
+    'team.title': 'Our Team',
+    'team.subtitle': "The Creatives Behind Amplify Media's Social Media Magic",
+    'team.cofounder': 'Co-Founder',
+    'team.mileDescription':
+      " With a keen eye for trends and a strong interest for social media, Mile creates bespoke advertising campaigns that amplify clients messages and drive results.",
+    'team.julianDescription':
+      'Julian is an incredible creative force, who brings a unique perspective to every project he works on. With a mission to amplify online success, he leads a team of creative minds and marketing experts who deliver results-driven solutions.',
+  },
+  fr: {
+    'nav.home': 'Accueil',
+    'nav.contact': 'Contactez nous',
+    'nav.lang': 'EN',
+  },
+} as const;
+
+export const showDefaultLang = false;
+
+export const routes = {
+  fr: {
+    contact: 'contacter',
+  },
+};
